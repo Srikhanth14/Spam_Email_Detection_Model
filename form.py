eb3e33012e93a1f19a0bf6707714a29b2f6ca4e4
SRIKHANTH R <139539680+Srikhanth14@users.noreply.github.com>
2024-01-01 17:54:02 +0530

    Update form.py

--- form.py
+++ form.py
@@ -27,11 +27,11 @@ def spam_form():
     
         # Display the prediction result
         if prediction[0] == 1:
-            st.write("Predicted Label: Spam mail")
+            st.error("Predicted Label: Spam mail")
             st.image(Image.open('Spam.jpg'), use_column_width=True)
             
         else:
-            st.write("Predicted Label: Ham mail")
+            st.success("Predicted Label: Ham mail")
             st.image(Image.open('good.jpg'), use_column_width=True)
             
     
