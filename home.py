be832ec7ee96439e1a31467fc2da614b52c5a93b
SRIKHANTH R <139539680+Srikhanth14@users.noreply.github.com>
2024-01-01 18:16:39 +0530

    Update home.py

--- home.py
+++ home.py
@@ -12,19 +12,19 @@ def spam_home():
     
     st.image(Image.open('home_img.jpg'),use_column_width=True)
     # Introduction
-    st.write("Welcome to the Spam Email Prediction App!")
+    st.subheader("Welcome to the Spam Email Prediction App!")
     st.write("Uncover the secrets of spam detection with advanced machine learning. This web app showcases the power of predictive analytics, providing accurate predictions to identify whether an email is spam or not.")
 
     # Key Features
     st.subheader("Key Features:")
     
-    st.write("1. **Spam Detection:**")
+    st.write("1.**Spam Detection:**")
     st.write("   Explore the capabilities of our spam detection model. It analyzes the content of emails to accurately predict whether they are spam or legitimate.")
 
-    st.write("2. **User-Friendly Interface:**")
+    st.write("2.**User-Friendly Interface:**")
     st.write("   Navigate effortlessly through our user-friendly interface. No expertise required – simply input your email message and witness the magic.")
 
-    st.write("3. **Predictive Insights:**")
+    st.write("3.**Predictive Insights:**")
     st.write("   Visualize the model's predictions and gain insights into the factors contributing to the classification of emails.")
 
     # About Me
